5f2a02444cd6b3c6e138a6a4f892535eb08105ed
Tom Wire <tom.w.wire@gmail.com>
2015-12-10 11:13:44 +0000

    use static files (for css and js)

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express");
-
 var app = express();
-
 var port = 5000;
+
+//static files
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 
 
 app.get('/', function(req, res){
