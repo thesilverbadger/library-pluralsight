9d07cab84e7e5bc31ec227c806fe6452c2b92b93
Tom Wire <tom.w.wire@gmail.com>
2016-01-12 12:49:35 +0000

    gulp-inject (although JS doesn't seem to work)

--- gulpfile.js
+++ gulpfile.js
@@ -14,13 +14,22 @@ gulp.task('style', function(){
 
 gulp.task('inject', function(){
     var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+    
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], { read: false });
+    
+    var injectOptions = {
+      ignorePath: '/public'  
+    };
     
     var options = {
         bowerJson: require('./bower.json'),
-        directory: './public/lib'
+        directory: './public/lib',
+        ignorePath: '../../public'
     }
     
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
 })
