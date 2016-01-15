71bfa09e937bb38a52b506ebd48d985eb4f20393
Tom Wire <tom.w.wire@gmail.com>
2016-01-15 12:57:05 +0000

    Use env for port (although this doesn't work...)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 //static files
 app.use(express.static('public'));
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var jsFiles = ['*.js', 'src/**/*.js'];
+var nodemon = require('gulp-nodemon');
 
 gulp.task('style', function(){
    return gulp.src(jsFiles)
@@ -26,10 +27,26 @@ gulp.task('inject', function(){
         bowerJson: require('./bower.json'),
         directory: './public/lib',
         ignorePath: '../../public'
-    }
+    };
     
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
-})
+});
+
+gulp.task('serve', ['style', 'inject'], function(){
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
+    
+    return nodemon(options)
+        .on('restart', function(ev){
+            console.log('Restarting...');
+        });
+});
