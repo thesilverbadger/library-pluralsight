4b68a0135d3a79540faab695de4281a3d6877021
Tom Wire <tom.w.wire@gmail.com>
2016-01-18 12:57:06 +0000

    Demo of Handlebars (not really suitable if you're also doing Angular)

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ var port = process.env.PORT || 5000;
 //static files
 app.use(express.static('public'));
 app.set('views', './src/views');
-app.set('view engine', 'jade');
+
+var handlebars = require('express-handlebars')
+app.engine('.hbs', handlebars({extname: '.hbs'}))
+app.set('view engine', '.hbs');
 
 app.get('/', function(req, res){
-	res.render('index', {list: ['a', 'b']});
+	res.render('index', {title: 'Hello from render', list: ['a', 'b']});
 });
 
 app.listen(port, function(err){
