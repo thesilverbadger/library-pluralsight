35bf1784fc2b4cc53b1ad8214afa04dbe7b162f6
Tom Wire <tom.w.wire@gmail.com>
2016-01-18 13:14:16 +0000

    Replace Handlebars with EJS

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ var port = process.env.PORT || 5000;
 app.use(express.static('public'));
 app.set('views', './src/views');
 
-var handlebars = require('express-handlebars')
-app.engine('.hbs', handlebars({extname: '.hbs'}))
-app.set('view engine', '.hbs');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
 	res.render('index', {title: 'Hello from render', list: ['a', 'b']});
