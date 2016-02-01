04e8c5b29ae0ea7b62581f3ed4e984a6d9af81d6
Tom Wire <tom.w.wire@gmail.com>
2016-02-01 12:53:03 +0000

    Talking to MSSQL (PW is wrong)

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 
+var sql = require('mssql');
+var config = {
+  user: 'library@cantiaci',
+  password: 'move_password_to_config',
+  server: 'cantiaci.database.windows.net',
+  database: 'library',
+  options: {
+      encrypt: true
+  }  
+};
+
+sql.connect(config, function(err){
+    if(err) {
+        console.log('connect error:' + err);
+    }
+});
+
+
+
 var nav = [{ 
                 link: '/books', 
                 text: 'Books' 
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,39 +1,59 @@
 var express = require('express');
 
 var bookRouter = express.Router();
+var sql = require('mssql'); //this gets the SAME instance as the one on app.js
 
 var router = function(nav){
-    var books = [
-    {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-    },
-    {
-        title: 'The Martian',
-        genre: 'SciFi',
-        author: 'Andy Weir',
-        read: true
-    }];
 
     bookRouter.route('/')
         .get(function(req, res){
-        res.render('bookListView', { 
-                title: 'books',
-                nav: nav,
-                books: books,
+            
+            var request = new sql.Request();
+            request.query('select * from books', function(err, recordset){
+                if(!err){
+                    res.render('bookListView', { 
+                        title: 'books',
+                        nav: nav,
+                        books: recordset,
+                    });
+                }
             });
+            
+            
         });
         
     bookRouter.route('/:id')
         .get(function(req, res){
-            var id = req.params.id;
-            res.render('bookView', { 
-                title: 'books',
-                nav: nav,
-                book: books[id],
+
+            var ps = new sql.PreparedStatement(/* [connection] */);
+            ps.input('id', sql.Int);
+            ps.prepare('select * from books where bookid = @id', function(err) {
+                // ... error checks 
+                if(err){
+                    console.log(err);
+                }
+                ps.execute({id: req.params.id}, function(err, recordset) {
+                    // ... error checks 
+                    if(err){
+                        console.log(err);
+                    }
+                    
+                    ps.unprepare(function(err) {
+                        // ... error checks 
+                        if(err){
+                            console.log(err);
+                        }
+                        console.log(recordset);
+
+                        res.render('bookView', { 
+                            title: 'books',
+                            nav: nav,
+                            book: recordset[0],
+                        });
+                    });
+                });
             });
+
         });
 
         return bookRouter;
